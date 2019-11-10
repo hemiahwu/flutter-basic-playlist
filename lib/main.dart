9af39837561becaa56f60fede8485e453e4de177
hemiahwu <309595700@qq.com>
2019-11-10 22:02:11 +0800

    lesson-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,22 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text("米斯特吴"),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text("Hello Flutter"),
+      child: Text(
+        "Hello Flutter",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: "IndieFlower"
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.red[600],
       child: Text("按钮"),
     ),
   )

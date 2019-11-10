ad0f256ed80634c9ee1af663bd5e20a39719c8de
hemiahwu <309595700@qq.com>
2019-11-10 21:07:56 +0800

    lesson-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Text("米斯特吴"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("米斯特吴"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Hello Flutter"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text("按钮"),
+    ),
+  )
 ));
 

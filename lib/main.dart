55ccef342f9e89742565e2d3ea4a09225f6d20eb
hemiahwu <309595700@qq.com>
2019-11-11 22:29:31 +0800

    lesson-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("米斯特吴"),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        "Hello Flutter",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: "IndieFlower"
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.red[600],
-      child: Text("按钮"),
-    ),
-  )
+  home: Home(),
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("米斯特吴"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: "IndieFlower"
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        child: Text("按钮3"),
+      ),
+    );
+  }
+}
+
+// widget: 设计不可变
+// 无状态widget: image
+// 有状态widget: checkbox

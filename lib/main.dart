326b54a3260bd5c1ec5729f9e8bcb2feb589bbc2
hemiahwu <309595700@qq.com>
2019-11-23 21:01:38 +0800

    lesson-13

--- lib/main.dart
+++ lib/main.dart
@@ -8,51 +8,80 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("米斯特吴"),
+        title: Text("米修在线"),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-              child: Image.asset('assets/image1.jpg'),
-              flex: 3,
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/me.jpg'),
+                  radius: 40.0,
+                ),
+              ),
+
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
+              ),
+              
+              Text("姓名",style: TextStyle(
+                color:Colors.grey,
+                letterSpacing: 2.0
+              )),
+              SizedBox(height: 10.0),
+              Text("米斯特吴",style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0
+              )),
+              SizedBox(height: 30.0),
+              Text("公司",style: TextStyle(
+                  color:Colors.grey,
+                  letterSpacing: 2.0
+              )),
+              SizedBox(height: 10.0),
+              Text("阿里巴巴,新零售",style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0
+              )),
+              SizedBox(height: 30.0),
+              Text("岗位",style: TextStyle(
+                  color:Colors.grey,
+                  letterSpacing: 2.0
+              )),
+              SizedBox(height: 10.0),
+              Text("Node.js开发",style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0
+              )),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color:Colors.grey[400]
+                  ),
+                  SizedBox(width: 10.0),
+                  Text('wuhaiyang@alibaba-inc.com',style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0
+                  ))
+                ],
+              ),
+            ],
           ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color:Colors.cyan,
-              child: Text('1'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color:Colors.amber,
-              child: Text('2'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color:Colors.red,
-              child: Text('3'),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[600],
-        child: Text("按钮3"),
       ),
     );
   }
 }
 
-// widget: 设计不可变
-// 无状态widget: image
-// 有状态widget: checkbox

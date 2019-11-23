37b1a9fe461eefcf2129471e55aba36969091384
hemiahwu <309595700@qq.com>
2019-11-23 21:45:05 +0800

    lesson-15

--- lib/main.dart
+++ lib/main.dart
@@ -10,91 +10,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int level = 2;
+
+  List<String> datas = [
+    "hello world",
+    "hello flutter",
+    "hello misterwu"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("米修在线"),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            level += 1;
-          });
-        },
-        backgroundColor: Colors.grey[800],
-        child: Icon(Icons.add),
-      ),
-      body: Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/me.jpg'),
-                  radius: 40.0,
-                ),
-              ),
 
-              Divider(
-                height: 60.0,
-                color: Colors.grey[800],
-              ),
+      body: Column(
+//        children: datas.map((data){
+//          return Text(data);
+//        }).toList()
 
-              Text("姓名",style: TextStyle(
-                color:Colors.grey,
-                letterSpacing: 2.0
-              )),
-              SizedBox(height: 10.0),
-              Text("米斯特吴",style: TextStyle(
-                color: Colors.amberAccent[200],
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0
-              )),
-              SizedBox(height: 30.0),
-              Text("公司",style: TextStyle(
-                  color:Colors.grey,
-                  letterSpacing: 2.0
-              )),
-              SizedBox(height: 10.0),
-              Text("阿里巴巴,新零售",style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28.0
-              )),
-              SizedBox(height: 30.0),
-              Text("等级",style: TextStyle(
-                  color:Colors.grey,
-                  letterSpacing: 2.0
-              )),
-              SizedBox(height: 10.0),
-              Text("$level",style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28.0
-              )),
-              SizedBox(height: 30.0),
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color:Colors.grey[400]
-                  ),
-                  SizedBox(width: 10.0),
-                  Text('wuhaiyang@alibaba-inc.com',style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0
-                  ))
-                ],
-              ),
-            ],
-          ),
+          children: datas.map((data) => Text(data)).toList()
+//        children: <Widget>[
+//          Text("hello world"),
+//          Text("hello flutter"),
+//          Text("hello misterwu"),
+//        ],
       ),
     );
   }

bcb6f95b517665fb190feed9710c30128f848465
hemiahwu <309595700@qq.com>
2019-11-26 19:41:16 +0800

    lesson-20

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,12 @@
 import 'package:flutter/material.dart';
-import 'datas.dart';
-import 'datas_card.dart';
+//import 'pages/home.dart';
+import 'package:myapp/pages/home.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
 ));
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
 
-
-class _HomeState extends State<Home> {
-
-  List<Datas> datas = [
-    Datas(text:"hello world",author:"米斯特吴"),
-    Datas(text:"hello flutter",author:"阿里巴巴"),
-    Datas(text:"hello missyou",author:"米修在线"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("米修在线"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-      ),
-
-      body: Column(
-          children: datas.map((data) => DataCard(
-            data:data,
-            delete:(){
-                setState(() {
-                  datas.remove(data);
-                });
-              },
-          )).toList()
-      ),
-    );
-  }
-}
 
 
 
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("choose location screen"),
+    );
+  }
+}

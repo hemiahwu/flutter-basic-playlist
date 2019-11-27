4828756c9a6d28199b3f6c5c4fa67d3cceee1449
hemiahwu <309595700@qq.com>
2019-11-27 11:57:20 +0800

    lesson-25

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,10 +12,17 @@ class _LoadingState extends State<Loading> {
   void getData() async {
 
     // http request
-    Response response = await get("https://jsonplaceholder.typicode.com/todos/1");
+    Response response = await get("http://worldtimeapi.org/api/timezone/Asia/Shanghai");
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+
+    // 创建时间对象
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
   }
 
   @override
